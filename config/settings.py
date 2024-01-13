8870f85e9153e4789c00ef423057ad630a2e5ff0
VladimirKovaliev <kovaliev.v@list.ru>
2024-01-13 16:16:41 +0300

    startproject cw6

--- config/settings.py
+++ config/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -75,8 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': 'postgres',
+        'HOST': '127.0.0.1',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD')
     }
 }
 
